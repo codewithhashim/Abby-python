f788ca5816fea39d295cfcb8a6ae12a119e5715e
prohashim <prohashim4e@gmail.com>
2023-11-02 00:30:48 +0500

    class 4

---
+++ class4/app.py
@@ -0,0 +1,32 @@
+# Expression: combination of values and operators that produce a value
+# operator (+) -> A symbol that performs some kind of operation on varaibles or values.
+
+# operation on values (2, 3)
+# 2 and 3 are the operands
+# print(2 + 3)
+
+abby_age = 45
+alex_age = 67
+# operation on variables
+# abby_age alex_age are operands.
+# print(abby_age + alex_age)
+# print(abby_age - alex_age)
+# division
+# print(abby_age / alex_age)
+# multiplication
+# print(abby_age * alex_age)
+
+# Binary Operator (The operators that combines two operands, all the above are binary operators)
+
+# Modulus operator (%)
+print("The remainder is: ", 12%7)
+
+
+# increment
+print(abby_age + 1)
+
+# decrement
+print(alex_age-1)
+
+# Exponent: 2 multiplied for three different times (2*2*2)
+print(2**3)
